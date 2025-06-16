59b6c9dee9f678ca6503c35610544df29a0bd73e
ebi0510 <ebi@okanoharunanoMacBook-Air.local>
2025-06-16 22:30:08 +0900

    初コミット

---
+++ index.ts
@@ -0,0 +1,11 @@
+let map: google.maps.Map;
+
+async function initMap(): Promise<void> {
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  map = new Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 35.6523016, lng: 139.7312395 },
+    zoom: 8,
+  });
+}
+
+initMap();
